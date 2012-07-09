56cd00eb221738ab943e391f397028a594199170
Guanjun He <gjhe@suse.com>
2012-07-08 19:50:33 -0700

    libceph: prevent the race of incoming work during teardown
    (cherry picked from commit a2a3258417eb6a1799cf893350771428875a8287)
    
    Add an atomic variable 'stopping' as flag in struct ceph_messenger,
    set this flag to 1 in function ceph_destroy_client(), and add the condition code
    in function ceph_data_ready() to test the flag value, if true(1), just return.
    
    Signed-off-by: Guanjun He <gjhe@suse.com>
    Reviewed-by: Sage Weil <sage@inktank.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ceph/ceph_common.c
+++ net/ceph/ceph_common.c
@@ -493,6 +493,8 @@ void ceph_destroy_client(struct ceph_client *client)
 {
 	dout("destroy_client %p\n", client);
 
+	atomic_set(&client->msgr.stopping, 1);
+
 	/* unmount */
 	ceph_osdc_stop(&client->osdc);
 
