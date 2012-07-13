c7d5293d9f6b20e8e200374231a6d098dc9d3f12
Todd Poynor <toddpoynor@google.com>
2012-07-12 20:27:16 -0700

    power: power_supply: move POWER_SUPPLY_PROP_USB_HC to type 'int' order
    Change-Id: I001af30ab5fe06dde5f368241f21b9e0864777a1
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -125,6 +125,8 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_TIME_TO_FULL_AVG,
 	POWER_SUPPLY_PROP_TYPE, /* use power_supply.type instead */
 	POWER_SUPPLY_PROP_SCOPE,
+	/* Local extensions */
+	POWER_SUPPLY_PROP_USB_HC,
 	/* Properties of type `const char *' */
 	POWER_SUPPLY_PROP_MODEL_NAME,
 	POWER_SUPPLY_PROP_MANUFACTURER,
