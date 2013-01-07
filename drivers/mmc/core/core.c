b1df8ae0b3687579e5dc2679d7dcf4dd9977ae47
Trey Ramsay <tramsay@linux.vnet.ibm.com>
2013-01-07 17:26:37 +0530

    mmc: core: Fix some driver hangs when dealing with broken devices
    There are infinite loops in the mmc code that can be caused by bad
    hardware.  The code will loop forever if the device never comes back
    from program mode, R1_STATE_PRG, and it is not ready for data,
    R1_READY_FOR_DATA.
    
    A long timeout is added to prevent the code from looping forever.
    The timeout will occur if the device never comes back from program
    state or the device never becomes ready for data.
    
    It's not clear whether the timeout will do more than log a pr_err()
    and then start a fresh hang all over again.  We may need to extend
    this patch later to perform some kind of reset of the device (is
    that possible?) or rejection of new I/O to the device.
    
    Change-Id: I1f3e5fddffb5134743fbfbcdef5e97803fd25fc6
    Signed-off-by: Trey Ramsay <tramsay@linux.vnet.ibm.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    Signed-off-by: Tatyana Brokhman <tlinder@codeaurora.org>
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2136,6 +2136,7 @@ static int mmc_do_erase(struct mmc_card *card, unsigned int from,
 {
 	struct mmc_command cmd = {0};
 	unsigned int qty = 0;
+	unsigned long timeout;
 	int err;
 
 	/*
@@ -2213,6 +2214,7 @@ static int mmc_do_erase(struct mmc_card *card, unsigned int from,
 	if (mmc_host_is_spi(card->host))
 		goto out;
 
+	timeout = jiffies + msecs_to_jiffies(MMC_CORE_TIMEOUT_MS);
 	do {
 		memset(&cmd, 0, sizeof(struct mmc_command));
 		cmd.opcode = MMC_SEND_STATUS;
@@ -2226,8 +2228,19 @@ static int mmc_do_erase(struct mmc_card *card, unsigned int from,
 			err = -EIO;
 			goto out;
 		}
+
+		/* Timeout if the device never becomes ready for data and
+		 * never leaves the program state.
+		 */
+		if (time_after(jiffies, timeout)) {
+			pr_err("%s: Card stuck in programming state! %s\n",
+				mmc_hostname(card->host), __func__);
+			err =  -EIO;
+			goto out;
+		}
+
 	} while (!(cmd.resp[0] & R1_READY_FOR_DATA) ||
-		 R1_CURRENT_STATE(cmd.resp[0]) == R1_STATE_PRG);
+		 (R1_CURRENT_STATE(cmd.resp[0]) == R1_STATE_PRG));
 out:
 	return err;
 }
