8e44522eba409e6dce5c0785e5e2c2355722ea3e
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2013-01-28 15:09:04 -0800

    power: pm8921-bms: Prevent SOC jumps while charging
    The BMS driver scales the SOC while charging based on the charge
    current. It does not scale when it sees that the battery voltage at
    terminals have fallen 10mV below the max current, the reason being a
    current spike that causes the voltage to droop.
    
    However there is a corner case when the voltage could droop but not by
    10mV. In this case the BMS will report a higher SOC value, since the
    charge current most likely has reduced.  This is incorrect.
    
    Fix this behavior by showing the previous SOC value under severe system
    load while charging. Note that when the vdd max adjusting algorithm in
    charger reduces the vdd max value, the battery terminal voltage reduces
    causing this scheme to treat it as a current spike. This is fine as it
    will scale SOC correctly the next time it does a calculation.
    
    Change-Id: I48ba7382b06ed90fb253dfbbaa4a406c1b8ce6eb
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/power/pm8921-bms.c
+++ drivers/power/pm8921-bms.c
@@ -173,6 +173,7 @@ struct pm8921_bms_chip {
 	int			disable_flat_portion_ocv;
 	int			ocv_dis_high_soc;
 	int			ocv_dis_low_soc;
+	int			prev_vbat_batt_terminal_uv;
 };
 
 /*
@@ -1733,6 +1734,8 @@ static int charging_adjustments(struct pm8921_bms_chip *chip,
 			pr_debug("CC_TO_CV ibat_ua = %d CHG SOC %d\n",
 					ibat_ua, soc);
 		}
+
+		chip->prev_vbat_batt_terminal_uv = vbat_batt_terminal_uv;
 		return soc;
 	}
 
@@ -1742,13 +1745,15 @@ static int charging_adjustments(struct pm8921_bms_chip *chip,
 	 */
 
 	/*
-	 * if voltage lessened by more than 10mV (possibly because of
+	 * if the battery terminal voltage lessened (possibly because of
 	 * a sudden increase in system load) keep reporting the prev chg soc
 	 */
-	if (vbat_batt_terminal_uv <= chip->max_voltage_uv - 10000) {
-		pr_debug("vbat_terminals %d < max = %d CC CHG SOC %d\n",
+	if (vbat_batt_terminal_uv < chip->prev_vbat_batt_terminal_uv) {
+		pr_debug("vbat_terminals %d < prev = %d CC CHG SOC %d\n",
 			vbat_batt_terminal_uv,
-			chip->max_voltage_uv, chip->prev_chg_soc);
+			chip->prev_vbat_batt_terminal_uv,
+			chip->prev_chg_soc);
+		chip->prev_vbat_batt_terminal_uv = vbat_batt_terminal_uv;
 		return chip->prev_chg_soc;
 	}
 
@@ -1775,6 +1780,7 @@ static int charging_adjustments(struct pm8921_bms_chip *chip,
 	}
 
 	pr_debug("Reporting CHG SOC %d\n", chip->prev_chg_soc);
+	chip->prev_vbat_batt_terminal_uv = vbat_batt_terminal_uv;
 	return chip->prev_chg_soc;
 }
 
