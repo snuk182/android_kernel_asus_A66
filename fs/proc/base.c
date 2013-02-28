4b1961d0396f8cc7d07d5843a9af5dbf9392ff18
Andrew Morton <akpm@linux-foundation.org>
2013-02-27 17:03:16 -0800

    fs/proc: clean up printks
    - use pr_foo() throughout
    
    - remove a couple of duplicated KERN_WARNINGs, via WARN(KERN_WARNING "...")
    
    - nuke a few warnings which I've never seen happen, ever.
    
    Cc: Joe Perches <joe@perches.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Change-Id: I2d6a3c6a4c1c0b862102f3dff8ea5814f7d245b5

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -73,6 +73,7 @@
 #include <linux/security.h>
 #include <linux/ptrace.h>
 #include <linux/tracehook.h>
+#include <linux/printk.h>
 #include <linux/cgroup.h>
 #include <linux/cpuset.h>
 #include <linux/audit.h>
