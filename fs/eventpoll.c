a3ab086a8f8dbd614f6c72814f3707001fdec25f
Colin Cross <ccross@android.com>
2013-05-06 23:50:16 +0000

    epoll: use freezable blocking call
    Avoid waking up every thread sleeping in an epoll_wait call during
    suspend and resume by calling a freezable blocking call.  Previous
    patches modified the freezer to avoid sending wakeups to threads
    that are blocked in freezable blocking calls.
    
    This call was selected to be converted to a freezable call because
    it doesn't hold any locks or release any resources when interrupted
    that might be needed by another freezing task or a kernel driver
    during suspend, and is a common site where idle userspace tasks are
    blocked.
    
    Change-Id: I848d08d28c89302fd42bbbdfa76489a474ab27bf
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Colin Cross <ccross@android.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -33,6 +33,7 @@
 #include <linux/bitops.h>
 #include <linux/mutex.h>
 #include <linux/anon_inodes.h>
+#include <linux/device.h>
 #include <linux/freezer.h>
 #include <asm/uaccess.h>
 #include <asm/io.h>
