5a8027d81a2237d4b1692de970c7198c6ecf108c
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2013-05-31 14:18:24 +0530

    msm: rotator: Add proper checks for enabling Fast YUV
    For rotator version 2.0 and above, there is support for fast
    YUV which gives better rotator performance. However, there are
    few constraints related to height under which fast YUV cannot be
    enabled when 90 degree rotation is enabled. Make sure
    to add proper checks before enabling fast YUV.
    
    Change-Id: Iafd204a6e4ef09e1151d01d8fde35359b7c3b7a5
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/char/msm_rotator.c
+++ drivers/char/msm_rotator.c
@@ -2428,6 +2428,7 @@ static int msm_rotator_start(unsigned long arg,
 				fast_yuv_en = !fast_yuv_invalid_size_checker(
 						info.rotations,
 						info.src.width,
+						info.src.height,
 						dst_w,
 						info.src.height,
 						dst_h,
