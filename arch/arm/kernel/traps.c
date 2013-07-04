4b08c917f461a2d300811ed0afc967a941bc2668
Russell King <rmk+kernel@arm.linux.org.uk>
2013-07-04 11:40:32 +0100

    ARM: move vector stubs
    Move the machine vector stubs into the page above the vector page,
    which we can prevent from being visible to userspace.  Also move
    the reset stub, and place the swi vector at a location that the
    'ldr' can get to it.
    
    This hides pointers into the kernel which could give valuable
    information to attackers, and reduces the number of exploitable
    instructions at a fixed address.
    
    Cc: <stable@vger.kernel.org>
    Acked-by: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Git-commit: 19accfd373847ac3d10623c5d20f948846299741
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    
    ARM: use linker magic for vectors and vector stubs
    
    Use linker magic to create the vectors and vector stubs: we can tell the
    linker to place them at an appropriate VMA, but keep the LMA within the
    kernel.  This gets rid of some unnecessary symbol manipulation, and
    have the linker calculate the relocations appropriately.
    
    Change-Id: I694f6b651e60fccd7e3c818f981a3883f9c212de
    Cc: <stable@vger.kernel.org>
    Acked-by: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Git-commit: b9b32bf70f2fb710b07c94e13afbc729afe221da
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [joonwoop@codeaurora.org: squashed 19accfd and b9b32bf because 19accfd
    			broke build]
    CRs-fixed: 561044
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -865,7 +865,7 @@ void __init early_trap_init(void *vectors_base)
 	 * are visible to the instruction stream.
 	 */
 	memcpy((void *)vectors, __vectors_start, __vectors_end - __vectors_start);
-	memcpy((void *)vectors + 0x200, __stubs_start, __stubs_end - __stubs_start);
+	memcpy((void *)vectors + 0x1000, __stubs_start, __stubs_end - __stubs_start);
 	memcpy((void *)vectors + 0x1000 - kuser_sz, __kuser_helper_start, kuser_sz);
 
 	/*
@@ -873,6 +873,6 @@ void __init early_trap_init(void *vectors_base)
 	 */
 	kuser_get_tls_init(vectors);
 
-	flush_icache_range(vectors, vectors + PAGE_SIZE);
+	flush_icache_range(vectors, vectors + PAGE_SIZE * 2);
 	modify_domain(DOMAIN_USER, DOMAIN_CLIENT);
 }
