f93ee2655bebe0c0fa298b6c2bd8ecaefb81c83d
Ravi Joshi <ravij@codeaurora.org>
2014-05-05 15:25:39 -0700

    msm: Create shadow files under /sys/kernel/ipv4
    The TCP delayed ack parameters can be configured from user space
    application which runs with non-root credentials and does not have
    direct write access to /proc entries. This commit creates shadow
    files under /sys/kernel/ipv4 which maps to /proc/sys/net/ipv4.
    
    CRs-Fixed: 628139
    Change-Id: If9f5e8f1f5f31c39090555b7b0c40efcdf6ccd08
    Signed-off-by: Ravi Joshi <ravij@codeaurora.org>

--- net/ipv4/sysfs_net_ipv4.c
+++ net/ipv4/sysfs_net_ipv4.c
@@ -132,6 +132,9 @@ CREATE_IPV4_FILE(tcp_rmem_min, sysctl_tcp_rmem[0]);
 CREATE_IPV4_FILE(tcp_rmem_def, sysctl_tcp_rmem[1]);
 CREATE_IPV4_FILE(tcp_rmem_max, sysctl_tcp_rmem[2]);
 
+CREATE_IPV4_FILE(tcp_delack_seg, sysctl_tcp_delack_seg);
+CREATE_IPV4_FILE(tcp_use_userconfig, sysctl_tcp_use_userconfig);
+
 static struct attribute *ipv4_attrs[] = {
 	&tcp_wmem_min_attr.attr,
 	&tcp_wmem_def_attr.attr,
@@ -140,6 +143,8 @@ static struct attribute *ipv4_attrs[] = {
 	&tcp_rmem_def_attr.attr,
 	&tcp_rmem_max_attr.attr,
 	&listen_port_attr.attr, //ASUS_BSP+ SYN FIREWALL
+	&tcp_delack_seg_attr.attr,
+	&tcp_use_userconfig_attr.attr,
 	NULL
 };
 
