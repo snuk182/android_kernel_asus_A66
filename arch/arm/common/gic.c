cd3329954cd6d23f7348c460b418366a85611dc2
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2014-06-18 13:53:36 -0700

    ARM: gic: Add support for logging interrupts in RTB
    To help with the debug of interrupt storms and problems during IRQ
    handling, log the IRQ number in RTB for each interrupt that fires.
    
    Change-Id: I479f8cc032b269f3d43d939419e346528d4f909e
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -50,6 +50,7 @@
 #include <asm/system.h>
 
 #include <mach/socinfo.h>
+#include <mach/msm_rtb.h>
 
 union gic_base {
 	void __iomem *common_base;
@@ -452,6 +453,7 @@ asmlinkage void __exception_irq_entry gic_handle_irq(struct pt_regs *regs)
 		if (likely(irqnr > 15 && irqnr < 1021)) {
 			irqnr = irq_find_mapping(gic->domain, irqnr);
 			handle_IRQ(irqnr, regs);
+			uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 			continue;
 		}
 		if (irqnr < 16) {
@@ -462,6 +464,7 @@ asmlinkage void __exception_irq_entry gic_handle_irq(struct pt_regs *regs)
 				raw_spin_unlock(&irq_controller_lock);
 #ifdef CONFIG_SMP
 			handle_IPI(irqnr, regs);
+			uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 #endif
 			continue;
 		}
