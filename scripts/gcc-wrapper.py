b7414f9e0f3bdd58b2e17da3ac5c71484fc2721f
Samir Mehta <samirn@codeaurora.org>
2015-09-15 17:09:55 +0530

    ARM: msm: GCC Version change.
    - This reverts commit 1e3bae22f3718341dea2d33c0a077a387537a6be.
    - Changing gcc toolchain to 4.9 version.
    - Fix linker error of unknown CPU architecture by
      adding CONFIG_MSM_CORTEX_A7 in the arch options
      for the GCC. The issue is seen after bump up to
      gcc-4.9.
    - Fix section mismatch errors after upgrading gcc to 4.9
      version. To fix these errors variables are moved to
      appropriate sections.
    
    Change-Id: I7b5c9212e63826a0cb5b7e4ab161737ae87a2157
    Signed-off-by: Samir Mehta <samirn@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/ext-buck-control.c
    	arch/arm/mach-msm/krait-regulator-pmic.c
    	arch/arm/mach-msm/krait-regulator.c
    	arch/arm/mach-msm/lpm_levels.c
    	arch/arm/mach-msm/msm-pm.c
    	arch/arm/mach-msm/rpm-smd.c
    	scripts/gcc-wrapper.py
    
    Change-Id: I7b7b950404ef876bf8b3d6a2366bfe67dc9cdc06

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -40,9 +40,6 @@ import subprocess
 # force LANG to be set to en_US.UTF-8 to get consistent warnings.
 
 allowed_warnings = set([
-    "alignment.c:327",
-    "mmu.c:602",
-    "return_address.c:62",
  ])
 
 # Capture the name of the object file, can find it.
