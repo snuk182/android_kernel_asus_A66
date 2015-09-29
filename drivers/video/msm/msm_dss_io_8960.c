4f530518bcd74d30629ba60e90e05fb7edbd7477
snuk182 <snuk188@gmail.com>
2015-09-29 14:55:40 +0300

    stucks at Looper start

--- drivers/video/msm/msm_dss_io_8960.c
+++ drivers/video/msm/msm_dss_io_8960.c
@@ -634,6 +634,12 @@ void cont_splash_clk_ctrl(int enable)
 {
 	static int cont_splash_clks_enabled;
 	if (enable && !cont_splash_clks_enabled) {
+			if (clk_set_rate(dsi_byte_div_clk, 1) < 0)      /* divided by 1 */
+				pr_err("%s: dsi_byte_div_clk - "
+					"clk_set_rate failed\n", __func__);
+			if (clk_set_rate(dsi_esc_clk, esc_byte_ratio) < 0) /* divided by esc */
+				pr_err("%s: dsi_esc_clk - "                      /* clk ratio */
+					"clk_set_rate failed\n", __func__);
 			clk_prepare_enable(dsi_byte_div_clk);
 			clk_prepare_enable(dsi_esc_clk);
 			cont_splash_clks_enabled = 1;
@@ -649,6 +655,14 @@ void mipi_dsi_prepare_clocks(void)
 	clk_prepare(amp_pclk);
 	clk_prepare(dsi_m_pclk);
 	clk_prepare(dsi_s_pclk);
+
+	if (clk_set_rate(dsi_byte_div_clk, 1) < 0)      /* divided by 1 */
+		pr_err("%s: dsi_byte_div_clk - "
+			"clk_set_rate failed\n", __func__);
+	if (clk_set_rate(dsi_esc_clk, esc_byte_ratio) < 0) /* divided by esc */
+		pr_err("%s: dsi_esc_clk - "                      /* clk ratio */
+			"clk_set_rate failed\n", __func__);
+
 	clk_prepare(dsi_byte_div_clk);
 	clk_prepare(dsi_esc_clk);
 }
@@ -706,6 +720,8 @@ void mipi_dsi_clk_enable(void)
 			"clk_set_rate failed\n", __func__);
 	mipi_dsi_pclk_ctrl(&dsi_pclk, 1);
 	mipi_dsi_clk_ctrl(&dsicore_clk, 1);
+	//clk_prepare_enable(dsi_byte_div_clk);
+	//clk_prepare_enable(dsi_esc_clk);
 	clk_enable(dsi_byte_div_clk);
 	clk_enable(dsi_esc_clk);
 	mipi_dsi_clk_on = 1;
